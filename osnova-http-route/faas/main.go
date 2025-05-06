ee551e57cd5e34f60e93330e2680a59fe50a3317
abdulaziz omonov <omonov2006omonov@gmail.com>
2025-05-06 13:57:39 +0500

    api add

---
+++ osnova-http-route/faas/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"function/pkg"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// Here is Azizbek's implementation of recoverMiddleware:
+
+var (
+	usage   = "run\n\nRuns a CloudFunction CloudEvent handler."
+	verbose = flag.Bool("V", false, "Verbose logging [$VERBOSE]")
+	port    = flag.Int("port", 8080, "Listen on all interfaces at the given port [$PORT]")
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, usage)
+		flag.PrintDefaults()
+	}
+	parseEnv()   // override static defaults with environment.
+	flag.Parse() // override env vars with flags.
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+// run a cloudevents client in receive mode which invokes
+// the user-defined function.Handler on receipt of an event.
+func run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		cancel()
+	}()
+
+	// Use a gorilla mux for handling all HTTP requests
+	router := chi.NewRouter()
+
+	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	// Add handlers for readiness and liveness endpoints
+	router.HandleFunc("/health/{endpoint:readiness|liveness}", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	cfg, _ := pkg.NewConfig()
+	params := pkg.NewParams(cfg)
+
+	httpServer := &http.Server{
+		Addr:           fmt.Sprintf(":%d", *port),
+		Handler:        router,
+		ReadTimeout:    1 * time.Minute,
+		WriteTimeout:   1 * time.Minute,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	listenAndServeErr := make(chan error, 1)
+	go func() {
+		if *verbose {
+			params.Log.Info().Msgf("Listening on :%d", *port)
+		}
+		err := httpServer.ListenAndServe()
+		cancel()
+		listenAndServeErr <- err
+	}()
+
+	<-ctx.Done()
+	shutdownCtx, shutdownCancelFn := context.WithTimeout(context.Background(), time.Second*5)
+	defer shutdownCancelFn()
+	err := httpServer.Shutdown(shutdownCtx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error on server shutdown: %v\n", err)
+	}
+
+	err = <-listenAndServeErr
+	if http.ErrServerClosed == err {
+		return nil
+	}
+	return err
+}
+
+// if the handler signature is compatible with http handler the function returns an instance of `http.Handler`,
+// otherwise nil is returned
+func toHttpHandler(handler interface{}, ctx context.Context) http.Handler {
+
+	if f, ok := handler.(func(rw http.ResponseWriter, req *http.Request)); ok {
+		return recoverMiddleware(http.HandlerFunc(f))
+	}
+
+	if f, ok := handler.(func(ctx context.Context, rw http.ResponseWriter, req *http.Request)); ok {
+		ff := func(rw http.ResponseWriter, req *http.Request) {
+			f(ctx, rw, req)
+		}
+		return recoverMiddleware(http.HandlerFunc(ff))
+	}
+
+	return nil
+}
+
+// parseEnv parses environment variables, populating the destination flags
+// prior to the builtin flag parsing.  Invalid values exit 1.
+func parseEnv() {
+	parseBool := func(key string, dest *bool) {
+		if val, ok := os.LookupEnv(key); ok {
+			b, err := strconv.ParseBool(val)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v is not a valid boolean\n", key)
+				os.Exit(1)
+			}
+			*dest = b
+		}
+	}
+	parseInt := func(key string, dest *int) {
+		if val, ok := os.LookupEnv(key); ok {
+			n, err := strconv.Atoi(val)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v is not a valid integer\n", key)
+				os.Exit(1)
+			}
+			*dest = n
+		}
+	}
+
+	parseBool("VERBOSE", verbose)
+	parseInt("PORT", port)
+}
---
+++ osnova-http-route/go.mod
@@ -0,0 +1,51 @@
+module function
+
+go 1.23.5
+
+toolchain go1.24.2
+
+require (
+	github.com/cloudevents/sdk-go/v2 v2.15.2
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-chi/cors v1.2.1
+	github.com/golanguzb70/redis-cache v1.1.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/joho/godotenv v1.5.1
+	github.com/kupalovmuhammadjon/mybazar-logger v1.1.2
+	github.com/kupalovmuhammadjon/rabbitmq-go v1.0.9-0.20250225095844-835ab004d54d
+	github.com/redis/go-redis/v9 v9.5.1
+	github.com/rs/zerolog v1.33.0
+	github.com/swaggo/http-swagger v1.3.4
+	github.com/ucode-io/ucode_sdk v1.0.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
